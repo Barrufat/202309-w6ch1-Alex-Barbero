404a9642aebb80ef6043ea52305afb5baa2e200b
Alex Barbero <barrufat@hotmail.com>
2023-10-18 18:56:17 +0200

    Add and type King

--- src/Components/King/King.ts
+++ src/Components/King/King.ts
@@ -9,7 +9,9 @@ export class King extends Character {
     this.yearsOfReign = yearsOfReign;
   }
 
-  communicate() {
+  communicate(): string {
     return `${super.communicate()}Everybody will die`;
   }
 }
+
+export default King;
---
+++ src/Components/charactersData.ts
@@ -0,0 +1,6 @@
+export const joffreyCharacterData = {
+  name: "Joffrey",
+  family: "Baratheon",
+  age: 16,
+  imageSource: "joffrey.jpg",
+};
