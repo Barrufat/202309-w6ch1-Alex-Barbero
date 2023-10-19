96b20e26d29d18fce7fad3e6bcd34b9adce0caeb
Alex Barbero <barrufat@hotmail.com>
2023-10-19 02:07:54 +0200

    Fix styles and character isAlive property

--- public/js/Components/CharacterCard/CharacterCard.js
+++ public/js/Components/CharacterCard/CharacterCard.js
@@ -1,24 +1,35 @@
 import Component from "../Component/Component.js";
 class CharacterCard extends Component {
   characterData;
-  constructor(parentElement, tagName, className, characterData) {
-    super(parentElement, tagName, className);
+  state;
+  constructor(parentElement, characterData, state) {
+    super(parentElement, "li", "col");
     this.characterData = characterData;
+    this.state = state;
   }
   populate() {
+    console.log(this.state);
     this.element.innerHTML = `
         <article class="character">
           <div class="card character__card">
-            <img src="img/${this.characterData.imageSource}" alt=${this.characterData.name} and ${this.characterData.family}" class="character__picture card-img-top" />
+            <img src="img/${this.characterData.imageSource}" alt=${
+              this.characterData.name
+            } and ${
+              this.characterData.family
+            }" class="character__picture card-img-top" />
             <div class="card-body">
-              <h2 class="character__name card-title h4">${this.characterData.name} & ${this.characterData.family}</h2>
+              <h2 class="character__name card-title h4">${
+                this.characterData.name
+              } & ${this.characterData.family}</h2>
               <div class="character__info">
                 <ul class="list-unstyled">
                   <li>Age: ${this.characterData.age}</li>
                   <li>
-                    State:
-                    <i class="fas fa-thumbs-down"></i>
-                    <i class="fas fa-thumbs-up"></i>
+                    State: ${
+                      this.state
+                        ? `<i class="fas fa-thumbs-up"></i>`
+                        : `<i class="fas fa-thumbs-down"></i>`
+                    }
                   </li>
                 </ul>
               </div>
--- src/Components/CharacterCard/CharacterCard.ts
+++ src/Components/CharacterCard/CharacterCard.ts
@@ -3,30 +3,40 @@ import Component from "../Component/Component.js";
 
 class CharacterCard extends Component {
   characterData;
+  state;
   constructor(
     parentElement: Element,
-    tagName: string,
-    className: string,
     characterData: CharacterDataType,
+    state: boolean,
   ) {
-    super(parentElement, tagName, className);
+    super(parentElement, "li", "col");
     this.characterData = characterData;
+    this.state = state;
   }
 
   protected populate(): void {
+    console.log(this.state);
     this.element.innerHTML = `
         <article class="character">
           <div class="card character__card">
-            <img src="img/${this.characterData.imageSource}" alt=${this.characterData.name} and ${this.characterData.family}" class="character__picture card-img-top" />
+            <img src="img/${this.characterData.imageSource}" alt=${
+              this.characterData.name
+            } and ${
+              this.characterData.family
+            }" class="character__picture card-img-top" />
             <div class="card-body">
-              <h2 class="character__name card-title h4">${this.characterData.name} & ${this.characterData.family}</h2>
+              <h2 class="character__name card-title h4">${
+                this.characterData.name
+              } & ${this.characterData.family}</h2>
               <div class="character__info">
                 <ul class="list-unstyled">
                   <li>Age: ${this.characterData.age}</li>
                   <li>
-                    State:
-                    <i class="fas fa-thumbs-down"></i>
-                    <i class="fas fa-thumbs-up"></i>
+                    State: ${
+                      this.state
+                        ? `<i class="fas fa-thumbs-up"></i>`
+                        : `<i class="fas fa-thumbs-down"></i>`
+                    }
                   </li>
                 </ul>
               </div>
